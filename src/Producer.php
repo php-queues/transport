09846e19d20745c43f14c9b9833297df3e32445e
v.zanfir <v.zanfir@corp.mail.ru>
2022-05-04 08:30:32 +0300

    Remove redundant Destination parameter from Producer::publish method

--- src/Producer.php
+++ src/Producer.php
@@ -5,14 +5,12 @@ declare(strict_types=1);
 namespace PhpQueues\Transport;
 
 /**
- * @psalm-template D as Destination
  * @psalm-template M as Message
  */
 interface Producer
 {
     /**
-     * @psalm-param D $destination
      * @psalm-param M ...$messages
      */
-    public function publish(Destination $destination, Message ...$messages): void;
+    public function publish(Message ...$messages): void;
 }
